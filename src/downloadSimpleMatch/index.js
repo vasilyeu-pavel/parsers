11d6312c379fb95b0c84db6616cd6269ac1a2526
Vasilyeu Pavel <vasilyuepavel@gmail.com>
2019-04-15 10:23:32 +0300

    fix output file name

--- src/downloadSimpleMatch/index.js
+++ src/downloadSimpleMatch/index.js
@@ -5,15 +5,15 @@ const downloader = async ({ url, name, options }) => {
         if (options.length) {
             await runCmdHandler(
                 './src/youtube-dl',
-                `youtube-dl ${options} ${url} --output RANDOM/${name}.mp4`);
+                `youtube-dl ${options} ${url} --output RANDOM/${name.replace(/ /g,'')}.mp4`);
         }else {
             await runCmdHandler(
                 './src/youtube-dl',
-                `youtube-dl ${url} --output RANDOM/${name}.mp4`);
+                `youtube-dl ${url} --output RANDOM/${name.replace(/ /g,'')}.mp4`);
         }
         sendTelegramMessage({
             league: 'RANDOM',
-            matches: [`${name}`,`${name}`],
+            matches: [`${name}`],
         });
         return;
     };
