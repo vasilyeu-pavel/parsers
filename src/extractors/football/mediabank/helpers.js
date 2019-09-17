0c86770257b1d4a296496d633c404c445e9ba466
Vasilyeu Pavel <vasilyuepavel@gmail.com>
2019-09-17 12:50:53 +0300

    create sports folders

---
+++ src/extractors/football/mediabank/helpers.js
@@ -0,0 +1,81 @@
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+const formatDate = require('../../../utils/formatDate');
+
+const cookiesParser = (coockies) => {
+    const arrFiltered = coockies.filter((el) => el.name === '_ga' || el.name === '__zlcmid' || el.name === 'Mediabank_' || el.name === 'server-id');
+
+    let str = '';
+    for (let i = 0; i < arrFiltered.length; i++) {
+        const elCockies = `${arrFiltered[i].name}=${arrFiltered[i].value}; `;
+        str += elCockies;
+    }
+
+    return str;
+};
+
+const getFormData = (form) => {
+    form.append('offset', '0');
+    form.append('limit', '300');
+    form.append('page', '1');
+    form.append('sortorder', '');
+    form.append('orderBy', '');
+    form.append('search', '');
+    form.append('smartsearchmatchtype', 'all');
+    form.append('smartsearchperiod', '');
+    form.append('smartsearchperiodtype', 'hours');
+    form.append('smartsearchassetcreatedfiltertype', 'before');
+    form.append('application', 'library');
+    form.append('smartsearchperiodenabled', 'false');
+    form.append('smartsearchassetcreatedfilterenabled', 'false');
+
+    return form;
+};
+
+const getMatchList = async (cookies, name, day) => {
+    const form = new FormData();
+    try {
+        const res = await fetch('https://www.mediabank.me/ajax/advanced_search.php', {
+            method: 'POST',
+            headers: {
+                Cookie: cookies
+            },
+            body: getFormData(form)
+        });
+
+        const { data: { assets } } = await res.json();
+
+        const filteredMatches = assets
+            .filter(({ assetmeta }) => assetmeta.SubTitle !== null)
+            .filter(({ assettitle, assetmeta }) => assettitle.includes('Half')
+                && assetmeta.EventDate === formatDate(day)
+                && assetmeta.FileStatus.toLowerCase() === 'ready')
+            .map(({ assetmeta, ...props }) => console.log(props) || ({
+                id: assetmeta.Id,
+                name: assetmeta.Title.replace(/ /g, '').replace(/\//g, ''),
+                date: assetmeta.EventDate,
+                league: assetmeta.League.replace(/ /g, '')
+            }));
+
+        return await Promise.all(filteredMatches.map(async (match) => {
+            const { id } = match;
+
+            const res = await fetch(`https://www.mediabank.me/download/?method=proxy_play&assetid=${id}&lang=`, {
+                method: 'GET',
+                headers: { Cookie: cookies }
+            });
+
+            return {
+                ...match,
+                url: res.url
+            };
+        }));
+    }
+    catch (e) {
+        throw new Error(`Ошибка в запросе к медиабанку ${e.message}`);
+    }
+};
+
+module.exports = {
+    cookiesParser, getMatchList
+};
---
+++ src/extractors/football/nasport/index.js
@@ -0,0 +1,113 @@
+const fetch = require('node-fetch');
+const moment = require('moment-timezone');
+const jsdom = require('jsdom');
+
+const formatDate = require('../../../utils/formatDate');
+const chunkArray = require('../../../utils/chunkArray');
+const config = require('../../../config');
+
+const { runCmdHandler } = require('../../../downloader');
+const { sendTelegramMessage } = require('../../../telegramBot');
+
+const getEventList = async (id, day) => {
+    const res = await fetch(`https://services.api.no/api/hjallis/v2/front?sport=${id}`);
+    const { entities: { events } } = await res.json();
+    if (!events || !Object.values(events).length) return;
+
+    const eventList = Object.values(events)
+        .filter((event) => moment(event.timespan.start).format('YYYY-MM-DD') === moment(day).format('YYYY-MM-DD'))
+        .filter((event) => event && event.game && event.game.league)
+        .map((event) => ({
+            name: event.title.trim().replace(/\s/g, ''),
+            start: moment(event.timespan.start).format('YYYY-MM-DD'),
+            id: event._id,
+            sport: event.sport.name,
+            league: config.nasport.sports[0].leagues[event.game.league._id]
+                && config.nasport.sports[0].leagues[event.game.league._id].replace(/\s/g, '').replace(/\-/g, '')
+        }))
+        .filter(({ league }) => league);
+
+    return eventList;
+};
+
+const getVideoId = async (eventId) => {
+    const res = await fetch(`https://services.api.no/api/hjallis/v2/event/${eventId}`);
+    const props = await res.json();
+    const { video } = props;
+
+    if (!video) return;
+    const { videoId } = video;
+    return { videoId };
+};
+
+const getVideoIds = (eventList) => new Promise((resolve) => {
+    Promise.all(eventList.map(async (event) => {
+        const video = await getVideoId(event.id);
+        if (!video) return {};
+        return ({ videoId: video.videoId, ...event });
+    }))
+        .then((events) => resolve(events));
+});
+
+const getLinkId = async (videoId) => {
+    const { JSDOM } = jsdom;
+    const res = await fetch(`https://ljsp.lwcdn.com/api/video/embed.jsp?id=${videoId}`);
+    const response = await res.text();
+
+    const dom = new JSDOM(response);
+    const container = dom.window.document.querySelector('#flowplayer-ovp-container');
+    if (container) {
+        return container.getAttribute('data-id');
+    }
+};
+
+const getLinkIds = (videoIds) => new Promise((resolve) => {
+    Promise.all(videoIds.map(async (event) => ({
+        ...event,
+        linkId: await getLinkId(event.videoId)
+    }))).then((res) => resolve(res));
+});
+
+const getUrls = async (id, day) => {
+    const eventList = await getEventList(id, day);
+    const videoIds = await getVideoIds(eventList);
+    console.log(eventList);
+
+    const events = await getLinkIds(videoIds.filter((videoId) => Object.keys(videoId).length));
+    return chunkArray(events.map(({
+        linkId, start, name, league
+    }) => ({
+        url: `https://lw-amedia-cf.lwcdn.com/hls/${linkId}/playlist.m3u8`,
+        name,
+        date: start,
+        ID: linkId,
+        league
+    })), 10);
+};
+
+const downloader = async (urls, parserName) => {
+    for (const chunkUrls of urls) {
+        await Promise.all(chunkUrls.map((url) => runCmdHandler(
+            './src/youtube-dl',
+            `youtube-dl --hls-prefer-native ${url.url} --output ${parserName}/${formatDate(url.date)}_${url.name.replace(/ /g, '')}.mp4`
+        )));
+        await sendTelegramMessage({
+            league: parserName,
+            matches: chunkUrls
+        });
+    }
+};
+
+const controller = async ({ id }, day) => await getUrls(id, day);
+
+const parser = async (browser, name, limit, day) => {
+    const { sports } = config[name];
+
+    for (const sport of sports) {
+        return await controller(sport, day);
+    }
+};
+
+module.exports = {
+    downloader, parser
+};
