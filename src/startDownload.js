e7015484b0703771920cdc4fe255c1cba3372cfc
Vasilyeu Pavel <vasilyuepavel@gmail.com>
2020-12-07 09:01:53 +0300

    фикс3

--- src/startDownload.js
+++ src/startDownload.js
@@ -5,13 +5,14 @@ const { runCmdHandler } = require('./utils/runCmdHandler');
 
 
 const startDownload = async () => {
+    const match = parseArgv(process.argv);
     const {
         name,
         url,
         date,
         league,
         options = "--hls-prefer-native"
-    } = parseArgv(process.argv);
+    } = match;
     console.log(`This process is pid ${process.pid}`);
 
     const savedName = `${league}/${formatDate(date)}_${name.replace(/ /g, '')}.mp4`;
@@ -20,7 +21,7 @@ const startDownload = async () => {
 
     await runCmdHandler('/parsers/src/youtube-dl', ydlCmd);
 
-    await sendTelegramMessage({ matches: [process.argv] });
+    await sendTelegramMessage({ matches: [match] });
 
     await delay(5000);
     console.log('Exiting.');
