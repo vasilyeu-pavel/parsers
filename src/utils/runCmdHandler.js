dcdf86095cdbc288a46933407ffcbdb2da2cbb83
pavas <pavas@zivitech.com>
2021-02-14 10:37:47 +0300

    remove support mac os and linux

--- src/utils/runCmdHandler.js
+++ src/utils/runCmdHandler.js
@@ -1,18 +1,19 @@
 const { spawn } = require('child_process');
 const os = require('os');
 
+const spawnWindowsProcess = (dir, cmd) => spawn(
+    'cmd.exe',
+    ['/c', ...cmd.split(' ')],
+    { cwd: dir, windowsVerbatimArguments: true }
+);
 
 const spawnProcess = (dir, cmd) => {
-    return (os.platform() === 'win32'
-        ? spawnWindowsProcess(dir, cmd)
-        : spawnLinuxProcess(dir, cmd));
-};
-
-const spawnWindowsProcess = (dir, cmd) => spawn('cmd.exe', ['/c', ...cmd.split(' ')], { cwd: dir, windowsVerbatimArguments: true });
-
-const spawnLinuxProcess = (dir, cmd) => {
-    const cmdParts = cmd.split(/\s+/);
-    return spawn(cmdParts[0], cmdParts.slice(1), { cwd: dir });
+    if (os.platform() !== 'win32') {
+        console.log('dir', dir);
+        console.log('cmd', cmd);
+        throw new Error('Is supported only windows os!');
+    }
+    return spawnWindowsProcess(dir, cmd);
 };
 
 const runCmdHandler = async (dir, cmd) => {
@@ -29,7 +30,7 @@ const runCmdHandler = async (dir, cmd) => {
             process.stderr.on('data', (data) => {
                 console.log('error', data.toString('utf-8'));
                 if (data.toString('utf-8').includes('Did not get any data blocks')) {
-                    reject(data.toString('utf-8'));
+                    reject(new Error(data.toString('utf-8')));
                 }
             });
             process.on('exit', (code, signal) => {
@@ -39,7 +40,7 @@ const runCmdHandler = async (dir, cmd) => {
         });
     }
     catch (e) {
-        if (e.includes('Did not get any data blocks')) {
+        if (e.message.includes('Did not get any data blocks')) {
             isRetry = true;
         }
         console.error(`Error trying to execute command '${cmd}' in directory '${dir}'`);
@@ -52,7 +53,8 @@ const runCmdHandler = async (dir, cmd) => {
         isRetry = false;
         console.log('Retry ---> download');
         runCmdHandler(dir, cmd);
-    } else {
+    }
+    else {
         return true;
     }
 };
