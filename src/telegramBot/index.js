cfd2a4e65e1a332277540f9505162c34cd91c09b
Vasilyeu Pavel <vasilyuepavel@gmail.com>
2021-03-07 18:54:23 +0300

    update tg bot api

--- src/telegramBot/index.js
+++ src/telegramBot/index.js
@@ -1,9 +1,11 @@
-const request = require('request');
+const TelegramBot = require('node-telegram-bot-api');
 
 const config = {
     token: '649626996:AAHuIMPw2xLUEgAoQgO6nM-v9rcwlQTVlEI',
     chatId: 405898308
 };
+
+const bot = new TelegramBot(config.token, { polling: true });
 
 const createMessage = ({ matches }) => {
     if (!matches) return 'matches - 0';
@@ -13,32 +15,10 @@ const createMessage = ({ matches }) => {
     `;
 };
 
-const sendTelegramMessage = (message) => new Promise((resolve, reject) => {
+const sendTelegramMessage = async (message) => {
     const messages = createMessage(message).replace(/,/g, '');
 
-    const options = {
-        method: 'GET',
-        url: `https://api.telegram.org/bot${config.token}/sendMessage`,
-        qs:
-            {
-                chat_id: config.chatId,
-                text: messages
-            },
-        headers:
-            {
-                Connection: 'keep-alive',
-                'Accept-Encoding': '',
-                'Accept-Language': 'en-US,en;q=0.8',
-                'cache-control': 'no-cache',
-                'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-            },
-        formData: { parse_mode: 'Markdown' }
-    };
-
-    request(options, (error, response, body) => {
-        if (error) reject(error);
-        resolve(body);
-    });
-});
+    return await bot.sendMessage(config.chatId, messages, { parse_mode: "markdown" });
+};
 
 module.exports = { sendTelegramMessage };
